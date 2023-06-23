da6b74560f938b17beecdb6f6d4e413a969a982f
PHETpierre <phpierre@hotmail.fr>
2023-06-23 17:29:48 +0200

    Changes to be committed: 	modified:   ScrollingShooter/main.lua

--- ScrollingShooter/main.lua
+++ ScrollingShooter/main.lua
@@ -1,9 +1,16 @@
 debug = true
 playerImg = nil
+canShoot = true
+canShootTimerMax = 0.2
+canShootTimer = canShootTimerMax
+
+bulletImg = nil
+bullets = {}
 
 function love.load(arg)
     player = { x = 200, y = 710, speed = 150, img = nil} 
     player.img = love.graphics.newImage('assets/plane.png')
+    bulletImg = love.graphics.newImage('assets/bullet_2_blue.png')
 end
 
 function love.update(dt)
@@ -12,9 +19,25 @@ function love.update(dt)
     end
 
     if love.keyboard.isDown('left', 'a') then
-        player.x = player.x - (player.speed*dt)
+        if player.x > 0 then
+            player.x = player.x - (player.speed*dt)
+        end
     elseif love.keyboard.isDown('right', 'd') then
-        player.x = player.x + (player.speed*dt)
+        if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+            player.x = player.x + (player.speed*dt)
+        end
+    end
+
+    canShootTimer = canShootTimer - (1 * dt)
+    if canShootTimer < 0 then
+        canShoot = true
+    end
+
+    if.love.keyboard.isDown('space', 'rctrl', 'lctrl') and canShoot then
+        newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
+        table.insert(bullets, newBullet)
+        canShoot = false
+        canShootTimer = canShootTimerMax
     end
 end
 
